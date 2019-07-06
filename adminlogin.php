60b8618c016dc45b83da5c10466ed960663af0d1
Amisha21 <52102466+Amisha21@users.noreply.github.com>
2019-07-06 09:39:17 -0700

    Create adminlogin.php

---
+++ adminlogin.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ADMIN - DASHBOARD</title>
+<style>
+body {
+   background-color: #aaaaaa;
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #444;
+}
+li {
+    float: left;
+    margin: 10px;
+}
+li a, .dropbtn {
+    display: inline-block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+li a:hover, .dropdown:hover .dropbtn {
+    background-color: #4CAF50;
+}
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+}
+#ima{
+margin-top: 200px;
+}
+.dropdown-content a:hover {background-color: #f1f1f1}
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+    h1{
+       color: teal;
+        font-weight: 600;
+        font-family: Montserrat,sans-serif;
+    }
+    h2{
+        color: tomato;
+        font-family: Montserrat,sans-serif;
+    }
+</style>
+</head>
+<body >
+
+<h1 align="center">Careworks Foundation</h1>
+<h2 align="center">Admin Homepage</h2>
+    
+<ul>
+    <center>
+        <li><a href="createevent.php">Create Event</a></li>
+  <li><a href="ongoing.php">Ongoing Event</a></li>
+  <li><a href="volunteer.php">All Events</a></li>
+  <li><a href="review_schools.php">All Volunteers</a></li>
+  <li><a href="review_schools.php">Volunteers Request</a></li>
+  <li><a href="data.php">Report Generation</a></li>
+  <li><a href="logout.php">Logout</a></li>
+</center>
+</ul>
+
+<div id="ima" align="center"><img src="logo.png" alt="background"></div>
+
+</div>
+</body>
+</html>
